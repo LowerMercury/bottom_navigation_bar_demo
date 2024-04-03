4b4dec065a5c67a977b311df9584b54832a39cad
Luke Kastern <luke.kastern@gmail.com>
2024-04-03 08:41:02 -0500

    Added essential dart files to make the code actually run as an app. Upcoming tournaments and _calander still doesn't seem to work even after fixes

---
+++ lib/upcoming_tournaments_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'match_details.dart';
+import 'tournament.dart';
+import '_calendar.dart';
+import 'volleymatic_model.dart';
+
+class UpcomingTournaments extends StatefulWidget {
+  final VolleymaticModel model; // Add this line to declare model as a property
+
+  const UpcomingTournaments({Key? key, required this.model}) : super(key: key);
+
+  @override
+  State<UpcomingTournaments> createState() => UpcomingTournamentsWidget();
+}
+
+
+class UpcomingTournamentsWidget extends State<UpcomingTournaments> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Upcoming Tournaments'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.calendar_today),
+            onPressed: () {
+              // Navigator.push(
+                // context,
+                // MaterialPageRoute(builder: (context) => Calendar(model: widget.model)), // Use widget.model here
+              // );
+            },
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: widget.model.tournaments.length, // Use widget.model.tournaments here
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            color: Colors.red,
+            child: ListTile(
+              onTap: () {
+                tapped(widget.model.tournaments[index]); // Use widget.model.tournaments here
+              },
+              title: Text(
+                widget.model.tournaments[index].name, // Use widget.model.tournaments here
+                style: const TextStyle(color: Colors.white)),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Date: ${widget.model.tournaments[index].date}', // Use widget.model.tournaments here
+                    style: const TextStyle(color: Colors.white)),
+                  Text(
+                    'Location: ${widget.model.tournaments[index].location}', // Use widget.model.tournaments here
+                    style: const TextStyle(color: Colors.white)),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+  
+  tapped(Tournament tournament) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const MatchDetails()));         
+  }
+}
+
